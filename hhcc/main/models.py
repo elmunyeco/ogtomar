3a3c3e651ea2ec05ac1792039aa9931cfc9e3a31
elmunyeco <elyecomun@gmail.com>
2024-08-30 22:12:15 -0300

    no se

--- hhcc/main/models.py
+++ hhcc/main/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class TipoDocumento(models.Model):
     nombre = models.CharField(max_length=50)
     descripcion = models.CharField(max_length=255)
@@ -8,14 +7,12 @@ class TipoDocumento(models.Model):
     def __str__(self):
         return self.nombre
 
-
 class Identificacion(models.Model):
     numero = models.CharField(max_length=20)
     tipo_documento = models.ForeignKey(TipoDocumento, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.tipo_documento.nombre}: {self.numero}"
-
 
 class Paciente(models.Model):
     SEXO_CHOICES = [
@@ -50,7 +47,6 @@ class Paciente(models.Model):
 
 from django.utils import timezone
 
-
 class HistoriaClinica(models.Model):
     paciente = models.OneToOneField(Paciente, on_delete=models.CASCADE)
     fechaalta = models.DateField(default=timezone.now)
@@ -58,20 +54,44 @@ class HistoriaClinica(models.Model):
     def __str__(self):
         return f"Historia Clínica de {self.paciente.nombre} {self.paciente.apellido}"
 
+class Visita(models.Model):
+    historia_clinica = models.ForeignKey(HistoriaClinica, on_delete=models.CASCADE)
+    fecha = models.DateField()
+    # otros campos relacionados a la visita
+    
+    
+class SignosVitales(models.Model):
+    visita = models.ForeignKey(Visita, on_delete=models.CASCADE)
+    peso = models.FloatField(null=True, blank=True)  # en kilogramos
+    glucemia = models.FloatField(null=True, blank=True)  # en mg/dL
+    colesterol = models.FloatField(null=True, blank=True)  # en mg/dL
+    presion_sistolica = models.IntegerField(null=True, blank=True)  # en mmHg
+    presion_diastolica = models.IntegerField(null=True, blank=True)  # en mmHg
 
-""" class Enfermedad(models.Model): """
-"""     nombre = models.CharField(max_length=100) """
-"""     descripcion = models.CharField(max_length=255) """
-"""  """
-"""     def __str__(self): """
-"""         return self.nombre """
+    def __str__(self):
+        return f"Signos Vitales de la visita {self.visita.id} para HC {self.visita.historia_clinica.id}"    
 
-"""  """
-""" class Antecedente(models.Model): """
-"""     enfermedad = models.ForeignKey(Enfermedad, on_delete=models.CASCADE) """
-"""     historia_clinica = models.ForeignKey(HistoriaClinica, on_delete=models.CASCADE) """
-"""     fecha = models.DateField() """
-"""     observaciones = models.CharField(max_length=255) """
-"""  """
-"""     def __str__(self): """
-"""         return f"{self.enfermedad.nombre} en {self.historia_clinica.paciente.nombre} {self.historia_clinica.paciente.apellido}" """
+class CondicionMedica(models.Model):
+    TIPO_CHOICES = [
+        ('enfermedad', 'Enfermedad'),
+        ('intervencion', 'Intervención'),
+        ('otro', 'Otro'),
+    ]
+    
+    orden = models.IntegerField(default=0)  # Campo para definir el orden de aparición
+
+    nombre = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=255, blank=True, null=True)
+    tipo = models.CharField(max_length=50, choices=TIPO_CHOICES, default='otro')
+
+    
+    def __str__(self):
+        return f"{self.nombre} ({self.tipo})"
+
+class Antecedente(models.Model):
+    condicion_medica = models.ForeignKey(CondicionMedica, on_delete=models.CASCADE)
+    historia_clinica = models.ForeignKey(HistoriaClinica, on_delete=models.CASCADE)
+    fecha = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.condicion.nombre} del tipo {self.condicion.tipo} en {self.historia_clinica.paciente.nombre} {self.historia_clinica.paciente.apellido}"
