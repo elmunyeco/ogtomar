0234ece4e8fe7dd38b37f0043102cb4cc0a26b2e
elmunyeco <elyecomun@gmail.com>
2024-10-15 19:43:23 -0300

    vp

--- post_migration_pacientes.py
+++ post_migration_pacientes.py
@@ -1,8 +1,14 @@
-#CORRER EN SHELL DE MANAGE PY. python manage.py shell -i ipython
 from main.models import Paciente, TipoDocumento
 
+# Recorremos todos los pacientes y actualizamos el valor de idTipoDoc basado en idTipoDoc_temp
 for paciente in Paciente.objects.all():
-    # Asignar el tipo de documento correcto en función del valor temporal
-    tipo_documento = TipoDocumento.objects.get(id=paciente.idTipoDoc_temp)
-    paciente.idTipoDoc = tipo_documento
-    paciente.save()
+    try:
+        # Buscamos el TipoDocumento con el ID que coincide con idTipoDoc_temp
+        tipo_documento = TipoDocumento.objects.get(id=paciente.idTipoDoc_temp)
+        
+        # Actualizamos el campo idTipoDoc_id directamente en la base de datos
+        Paciente.objects.filter(id=paciente.id).update(idTipoDoc=tipo_documento)
+        
+    except TipoDocumento.DoesNotExist:
+        # Si el tipo de documento no existe, se puede manejar el error, por ejemplo:
+        print(f"TipoDocumento con id {paciente.idTipoDoc_temp} no existe para el paciente {paciente.nombre} {paciente.apellido}")
