8d5374c9e73f8ed69b9215d4492a049e44406a53
elmunyeco <elyecomun@gmail.com>
2024-10-16 19:02:02 -0300

    pacientes_migration y pacientes2

--- post_migration_pacientes.sql
+++ post_migration_pacientes.sql
@@ -1,3 +1,9 @@
 ALTER TABLE pacientes ALTER COLUMN idTipoDoc_id SET DEFAULT 1;
-ALTER TABLE pacientes ADD FULLTEXT INDEX nombre_apellido_idx (nombre, apellido);
-ALTER TABLE pacientes ADD COLUMN idTipoDoc_temp INT(10);
+ALTER TABLE pacientes ADD FULLTEXT INDEX nombre_apellido_fulltext_idx (nombre, apellido);
+ALTER TABLE pacientes DROP FOREIGN KEY pacientes_idTipoDoc_id_6d4a5435_fk_tipos_documentos_id;
+ALTER TABLE pacientes DROP INDEX pacientes_idTipoDoc_id_numDoc_d5b9acca_uniq;
+-- cargar las tablas tipos_documentos, pacientes e historias_clinicas
+-- correr post_migration_pacientes.py
+ALTER TABLE pacientes ADD CONSTRAINT pacientes_idTipoDoc_id_numDoc_d5b9acca_uniq UNIQUE (idTipoDoc_id, numDoc);
+ALTER TABLE pacientes ADD CONSTRAINT pacientes_idTipoDoc_id_6d4a5435_fk_tipos_documentos_id FOREIGN KEY (idTipoDoc_id) REFERENCES tipos_documentos(id);
+-- Borrar idTipoDoc_temp desde el model y migrar.
