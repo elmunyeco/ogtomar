cbf0fb5671ac4b2dda745710712e4e117f6b0cd6
elmunyeco <elyecomun@gmail.com>
2024-11-04 18:10:57 -0300

    para ver

--- runmariadb.sh
+++ runmariadb.sh
@@ -1,2 +1,2 @@
-  docker run --name mariadb -e MYSQL_ROOT_PASSWORD=hhcc -p 3306:3306 -v /home/eze/hhcc_data:/var/lib/mysql -d mariadb:latest
   docker run -p 0.0.0.0:3307:3306 --name nuevo_cardioprieto -e MARIADB_ROOT_PASSWORD=Corbis5 -d  mariadb:latest
+  docker run --name phpmyadmin -d --link nuevo_cardioprieto:cardioprieto -e MYSQL_ROOT_PASSWORD=Corbis5 -p 9090:80 phpmyadmin
