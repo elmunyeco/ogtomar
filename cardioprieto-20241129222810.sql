031bec581ee4f12e74ead122d783ea94dbff9e68
elmunyeco <elyecomun@gmail.com>
2024-12-04 18:07:51 -0300

    cardioprieto-20241129222810.sql con un typo corregido

--- cardioprieto-20241129222810.sql
+++ cardioprieto-20241129222810.sql
@@ -1,4 +1,4 @@
-/*M!999999\- enable the sandbox mode */ 
+/*M!999999 enable the sandbox mode */ 
 -- MariaDB dump 10.19-11.4.3-MariaDB, for Linux (x86_64)
 --
 -- Host: 127.0.0.1    Database: cardioprieto
