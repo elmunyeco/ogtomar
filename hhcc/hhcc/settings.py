1bf03a5a74c99c7765ecca853872d1e94e920e7d
elmunyeco <elyecomun@gmail.com>
2026-03-31 17:30:10 -0300

    2026-03-31 17:30:10 auth mostrar usuario y password simple

--- hhcc/hhcc/settings.py
+++ hhcc/hhcc/settings.py
@@ -116,20 +116,9 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
+# En local, permitimos contraseñas simples para evitar bloqueos en UI.
+# Ajustar/endurcer antes de producción.
+AUTH_PASSWORD_VALIDATORS = []
 
 
 # Internationalization
